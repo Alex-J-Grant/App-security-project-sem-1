02be3e28c550711c4cf43dc5419be38ff51abeb3
Waheyy <harutoakane007@gmail.com>
2025-08-07 10:01:26 +0800

    i forgot what i did 2fa forget password admin dashboard banning

--- app.py
+++ app.py
@@ -14,11 +14,8 @@ from routes.profile import *
 from routes.search import *
 from routes.friends import friends  # ADD THIS LINE
 from routes.users import users  # ADD THIS LINE
-<<<<<<< HEAD
 from routes.pages import pages
-=======
 from routes.admin import adminbp
->>>>>>> weifeng
 from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
 from flask_talisman import Talisman
@@ -93,18 +90,6 @@ def create_app():
     sess = Session(app)
     # Enable CSRF protection
     csrf = CSRFProtect(app)
-<<<<<<< HEAD
-    app.config.update(
-        MAIL_SERVER='smtp.gmail.com',
-        MAIL_PORT=587,
-        MAIL_USE_SSL=False,
-        MAIL_USE_TLS=True,
-        MAIL_USERNAME=os.getenv('EMAIL'),
-        MAIL_PASSWORD=os.getenv('EMAIL_PASSWORD'),
-        MAIL_DEFAULT_SENDER=os.getenv('EMAIL'),
-    )
-=======
->>>>>>> weifeng
     mail.init_app(app)
 
     limiter.init_app(app)
@@ -137,4 +122,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True)
+    app.run(ssl_context=('cert.pem', 'key.pem'))
